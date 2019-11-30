2f797657d8d7bf16ef04f0a24d7041795dff1cb2
Nejm <nejmeddine.khechine@esprit.tn>
2019-11-30 13:33:53 +0100

    dynamic page crawl

--- index.py
+++ index.py
@@ -56,35 +56,56 @@ def query_example():
 
 @app.route('/searchcontent', methods=['GET'])
 def content():
-    array= []
-    url= request.args.get('url')
-    response = requests.get(url)
+    listprod= []
+    array1= []
+    url1= request.args.get('url')
+    pageurl= request.args.get('pageurl')
+    pageN= int(request.args.get('pagen'))
+    print pageN
+    if (pageN>1):
+        for i in range(1,pageN):
+            fullurl=url1+pageurl+str(i)
+            #print fullurl
+            response = requests.get(fullurl)
+            soup = BeautifulSoup(response.text, "html.parser")
+            for a in soup.findAll('a', {'class': 'product-name'}):
+                array1.append( a['href'])
+    else:
+        response = requests.get(url1)
+        soup = BeautifulSoup(response.text, "html.parser")
+        for a in soup.findAll('a', {'class': 'product-name'}):
+            array1.append( a['href'])
+
     titletag= request.args.get('titletag')
     imagetag= request.args.get('imagetag')
     reftag= request.args.get('reftag')
     desctag= request.args.get('desctag')
-    soup = BeautifulSoup(response.text, "html.parser")
-    if(titletag=='id'):
-        array.append(soup.find(id=request.args.get('title')))
-    elif(titletag=='h1'):
-        array.append(soup.findAll('h1')[0].text.strip())
-    elif(titletag=='h2'):
-        array.append(soup.findAll('h2')[0].text.strip())
-    else:
-        array.append(soup.findAll("div", {"class": request.args.get('title')}))
-    if(reftag=='id'):
-        array.append(soup.find(id=request.args.get('ref')).text)
-    else:
-        array.append(soup.findAll("div", {"class": request.args.get('ref')}))
-    if(imagetag=='id'):
-        array.append(soup.find(id=request.args.get('image'))['src'])
-    else:
-        array.append(soup.findAll("div", {"class": request.args.get('image')})['src'])
-    if(desctag=='id'):
-        array.append(soup.find(id=request.args.get('desc')))
-    else:
-        array.append(soup.findAll("div", {"class": request.args.get('desc')}))
-    return render_template('content.html',len=len(array),soup=array)
+    for url in array1:
+        response = requests.get(url)
+        prod= []
+        soup = BeautifulSoup(response.text, "html.parser")
+        if(titletag=='id'):
+            prod.append(soup.find(id=request.args.get('title')))
+        elif(titletag=='h1'):
+            prod.append(soup.findAll('h1')[0].text.strip())
+        elif(titletag=='h2'):
+            prod.append(soup.findAll('h2')[0].text.strip())
+        else:
+            prod.append(soup.findAll("div", {"class": request.args.get('title')}))
+        if(reftag=='id'):
+            prod.append(soup.find(id=request.args.get('ref')).text)
+        else:
+            prod.append(soup.findAll("div", {"class": request.args.get('ref')}))
+        if(imagetag=='id'):
+            prod.append(soup.find(id=request.args.get('image'))['src'])
+        else:
+            prod.append(soup.findAll("div", {"class": request.args.get('image')})['src'])
+        if(desctag=='id'):
+            prod.append(soup.find(id=request.args.get('desc')))
+        else:
+            prod.append(soup.findAll("div", {"class": request.args.get('desc')}))
+        listprod.append(prod)
+    return render_template('content.html',len=len(listprod),listprod=listprod)
 
 
 
