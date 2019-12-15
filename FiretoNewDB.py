0ee434e84229715176c17d22a296c994e962b44d
Nejm <nejmeddine.khechine@esprit.tn>
2019-12-15 18:03:13 +0100

    Ftomongo

--- FiretoNewDB.py
+++ FiretoNewDB.py
@@ -1,11 +1,13 @@
 import json
-import urllib.request
+#import urllib.request
 
 def download(url):
     print('Beginning file download with urllib2...')
     path ='/var/www/bsmart_admin/server/uploads/brands'
-    urllib.request.urlretrieve(url, path)
-    return {'path' : path, 'name': url}
+    #urllib.request.urlretrieve(url, path)
+    file= str(url).split("/o/")
+    print file[1]
+    #return {'path' : path, 'name': url}
 
 with open('brds.json') as json_file:
     data = json.load(json_file)
@@ -14,9 +16,16 @@ json_file.close()
 
 
 for a in data:
-    tmp= a['url']
-    a['image']= download(tmp)
+    #print a['url']
+    download(a['url'])
+
+    #a['image']= download(tmp)
+    a.pop('wasted', None)
+    a.pop('ctgr', None)
+    a.pop('provider', None)
+    a.pop('user', None)
     data=a
+
     jsonFile = open("brds.json", "w+")
     jsonFile.write(json.dumps(data))
     jsonFile.close()
