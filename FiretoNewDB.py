4647b486dc3f1d13fd7d82f05e8f15de9f9720b7
Nejm <nejmeddine.khechine@esprit.tn>
2019-12-15 19:06:46 +0100

    final firebase to new Mongo

--- FiretoNewDB.py
+++ FiretoNewDB.py
@@ -1,31 +1,36 @@
 import json
-#import urllib.request
+import requests
 
 def download(url):
     print('Beginning file download with urllib2...')
-    path ='/var/www/bsmart_admin/server/uploads/brands'
-    #urllib.request.urlretrieve(url, path)
     file= str(url).split("/o/")
-    print file[1]
-    #return {'path' : path, 'name': url}
+    ext1= file[1].split(".")
+    ext= ext1[1].split("?")[0]
+    ff= file[1].split("&token=")
+    filename=ff[1]+'.'+ext
+    path= str('/var/www/bsmart_admin/server/uploads/brands')+str(filename)
+    headers = {'user-agent': 'test-app/0.0.1'}
+    r = requests.get(url, headers=headers)
+    open(path, 'wb').write(r.content)
+    aa= {'path' : 'brands/'+filename, 'name': filename}
+    return aa
 
 with open('brds.json') as json_file:
     data = json.load(json_file)
 
 json_file.close()
-
+data1= []
 
 for a in data:
-    #print a['url']
-    download(a['url'])
-
-    #a['image']= download(tmp)
+    url= a['url']
+    #download(url)
+    a['image']= download(url)
     a.pop('wasted', None)
     a.pop('ctgr', None)
     a.pop('provider', None)
     a.pop('user', None)
-    data=a
+    data1.append(a)
 
-    jsonFile = open("brds.json", "w+")
-    jsonFile.write(json.dumps(data))
-    jsonFile.close()
+jsonFile = open("brds_array.json", "w+")
+jsonFile.write(json.dumps(data1))
+jsonFile.close()
