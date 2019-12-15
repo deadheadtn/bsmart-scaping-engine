e772e0102567af1fb80d5325855d33e9055a469d
Nejm <nejmeddine.khechine@esprit.tn>
2019-12-15 20:55:52 +0100

    providers

--- FiretoNewDB.py
+++ FiretoNewDB.py
@@ -1,6 +1,5 @@
 import json
 import requests
-from datetime import datetime
 
 def download(url):
     print('Beginning file download with urllib2...')
@@ -9,35 +8,35 @@ def download(url):
     ext= ext1[1].split("?")[0]
     ff= file[1].split("&token=")
     filename=ff[1]+'.'+ext
-    path= str('/var/www/bsmart_admin/server/uploads/brands/')+str(filename)
+    path= str('/var/www/bsmart_admin/server/uploads/providers/')+str(filename)
     headers = {'user-agent': 'test-app/0.0.1'}
     r = requests.get(url, headers=headers)
     open(path, 'wb').write(r.content)
-    aa= {'path' : 'brands/'+filename, 'name': filename}
+    aa= {'path' : 'providers/'+filename, 'name': filename}
     return aa
 
-with open('../brds_array1.json') as json_file:
+with open('../Providers_array.json') as json_file:
     data = json.load(json_file)
 
 json_file.close()
 data1= []
 
 for a in data:
-    #url= a['url']
-    #download(url)
-    #a['image']= download(url)
-    #a.pop('wasted', None)
-    #a.pop('ctgr', None)
-    #a.pop('provider', None)
-    #a.pop('user', None)
-    timestamp = datetime.timestamp(now)
-    a['created_at']=timestamp
+    url= a['url']
+    download(url)
+    a['image']= download(url)
+    a.pop('wasted', None)
+    a.pop('ctgr', None)
+    a.pop('role', None)
+    a.pop('user', None)
+    a['created_at']=11111
+     if a.get('desc') is  None:
+        a['description'] = ''
+    else:
+        a['description']= a.get('desc').encode("utf-8")
+    a.pop('desc', None)
     data1.append(a)
 
-jsonFile = open("../brds_array.json", "w+")
+jsonFile = open("../Providers_array1.json", "w+")
 jsonFile.write(json.dumps(data1))
 jsonFile.close()
-
-
-
-/usr/bin/mongoimport --username  seif --password test1234 --host 51.77.147.246 --db bsmart2  --collection Brand --jsonArray --file brds_array1.json
