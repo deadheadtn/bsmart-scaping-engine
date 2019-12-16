638ef3509550af52883ac83763e554633524b990
Nejm <nejmeddine.khechine@esprit.tn>
2019-12-16 04:09:02 +0100

    update

--- index.py
+++ index.py
@@ -189,7 +189,7 @@ def content():
                 prod.append(soup.findAll("div", {"class": request.args.get('desc')}).text)
             listprod.append(prod)
         #return render_template('content.html',len=len(listprod),listprod=listprod)
-            imaage=download(str(prod[2]))
+            imaage=download(prod[2])
             #jsonprod= {"name": str(prod[0])}
             jsonprod= {"name": str(prod[0]),"reference": prod[1], "image": imaage ,"description": prod[3],"providers": str(provider), "category": str(cat),"subcategory": str(subcat)}
             print jsonprod
