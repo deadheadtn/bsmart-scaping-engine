efbcc357725a7dba62409c891d8e4648083f9dfc
Nejm <nejmeddine.khechine@esprit.tn>
2019-12-16 04:18:04 +0100

    update

--- index.py
+++ index.py
@@ -191,7 +191,7 @@ def content():
         #return render_template('content.html',len=len(listprod),listprod=listprod)
             imaage=download(prod[2])
             #jsonprod= {"name": str(prod[0])}
-            jsonprod= {"name": str(prod[0]),"reference": prod[1], "image": imaage ,"description": prod[3],"providers": str(provider), "category": str(cat),"subcategory": str(subcat)}
+            jsonprod= {"name": str(prod[0]),"reference": prod[1], "images": imaage ,"description": prod[3],"providers": str(provider), "category": str(cat),"subcategory": str(subcat)}
             print jsonprod
             x = db.products.insert_one(jsonprod)
             print x
